e747fd8806141c75fbb8c0fbe2de3965f98d2940
Gewi413 <41789317+Gewi413@users.noreply.github.com>
2020-06-10 15:42:20 +0200

    made hitboxes use Polygons for simpler code

--- src/main/java/engine/mechanics/Hitbox.java
+++ src/main/java/engine/mechanics/Hitbox.java
@@ -3,15 +3,14 @@ package engine.mechanics;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
 
 /**
  * Use this class to create an hitbox.
  */
 public class Hitbox {
 
-    public Shape shape;
-    private Shape original;
+    public Polygon shape;
+    private Polygon original;
 
     /**
      * The constructor of the hitbox class. It takes an infinite amount of points which represent an n-shape. If only two
@@ -20,17 +19,20 @@ public class Hitbox {
      * @param points The points in form of a dimension.
      */
     public Hitbox(Point... points) {
+        shape = new Polygon();
         switch (points.length) {
             case 0:
             case 1:
                 throw new IllegalArgumentException("invalid number of Points");
             case 2:
-                shape = new Rectangle(points[0], new Dimension(points[1].x - points[0].x, points[1].y - points[0].y));
+                shape.addPoint(points[0].x, points[0].y);
+                shape.addPoint(points[1].x, points[0].y);
+                shape.addPoint(points[1].x, points[1].y);
+                shape.addPoint(points[0].x, points[1].y);
                 return;
             default:
-                shape = new Polygon();
                 for (Point p : points) {
-                    ((Polygon) shape).addPoint(p.x, p.y);
+                    shape.addPoint(p.x, p.y);
                 }
         }
     }
@@ -51,30 +53,21 @@ public class Hitbox {
      */
     public boolean isInside(Hitbox test) {
         if (!shape.getBounds2D().intersects(test.shape.getBounds2D())) return false;
-        if (shape instanceof Rectangle && test.shape instanceof Rectangle) return true;
-        Polygon poly = null;
-        if (shape instanceof Polygon && test.shape instanceof Polygon) {
-            poly = (Polygon) shape;
-            Polygon other = (Polygon) test.shape;
-            for (int i = 0; i < other.npoints; i++)
-                if (poly.contains(other.xpoints[i], other.ypoints[i])) return true;
-            for (int i = 0; i < poly.npoints; i++)
-                if (other.contains(poly.xpoints[i], poly.ypoints[i])) return true;
-            return false;
-        }
 
-        Rectangle2D rect = null;
-        if (shape instanceof Rectangle) {
-            poly = (Polygon) test.shape;
-            rect = (Rectangle2D) shape;
-        }
-        if (test.shape instanceof Rectangle) {
-            assert shape instanceof Polygon;
-            poly = (Polygon) shape;
-            rect = (Rectangle2D) test.shape;
-        }
-        if (rect == null) throw new NullPointerException("wtf did you do to achieve this?");
-        return poly.intersects(rect);
+        return isInside(shape, test.shape) || isInside(test.shape, shape);
+    }
+
+    /**
+     * internal method to check if points of one polygon are inside of another
+     *
+     * @param a the points to be checked
+     * @param b the polygon in which they should be checked
+     * @return if points of a lie within b
+     */
+    private boolean isInside(Polygon a, Polygon b) {
+        for (int i = 0; i < a.npoints; i++)
+            if (b.contains(a.xpoints[i], a.ypoints[i])) return true;
+        return false;
     }
 
     /**
@@ -85,15 +78,9 @@ public class Hitbox {
      * @param y The y pos you want to move in.
      */
     public void move(int x, int y) {
-        if (shape instanceof Polygon) {
-            Polygon poly = (Polygon) shape;
-            int xDiff = x - poly.xpoints[0];
-            int yDiff = y - poly.ypoints[0];
-            poly.translate(xDiff, yDiff);
-            return;
-        }
-        if (shape instanceof Rectangle)
-            ((Rectangle) shape).setLocation(x, y);
+        int xDiff = x - shape.xpoints[0];
+        int yDiff = y - shape.ypoints[0];
+        shape.translate(xDiff, yDiff);
     }
 
     /**
@@ -102,29 +89,23 @@ public class Hitbox {
      * @param angle the angle to rotate by in radians
      */
     public void rotate(double angle) {
-        if (shape instanceof Rectangle) {
-            Rectangle rect = (Rectangle) shape;
-            Point p1 = new Point(rect.x, rect.y);
-            Point p2 = new Point(rect.x + rect.width, rect.y + rect.height);
-            Polygon poly = new Polygon();
-            poly.addPoint(p1.x, p1.y);
-            poly.addPoint(p2.x, p1.y);
-            poly.addPoint(p2.x, p2.y);
-            poly.addPoint(p1.x, p2.y);
-            shape = poly;
+        if (original == null)
+            original = new Polygon(shape.xpoints, shape.ypoints, shape.npoints);
+        shape = new Polygon();
+        AffineTransform transform = AffineTransform.getRotateInstance(angle);
+        for (int i = 0; i < original.npoints; i++) {
+            Point2D p = transform.transform(new Point(original.xpoints[i], original.ypoints[i]), null);
+            shape.addPoint((int) p.getX(), (int) p.getY());
         }
-        if (shape instanceof Polygon) {
-            Polygon poly = (Polygon) shape;
-            Polygon next = new Polygon();
-            if (original == null)
-                original = new Polygon(poly.xpoints, poly.ypoints, poly.npoints);
-            Polygon orig = (Polygon) original;
-            AffineTransform transform = AffineTransform.getRotateInstance(angle);
-            for (int i = 0; i < poly.npoints; i++) {
-                Point2D p = transform.transform(new Point(orig.xpoints[i], orig.ypoints[i]), null);
-                next.addPoint((int) p.getX(), (int) p.getY());
-            }
-            shape = next;
-        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder("Hitbox{");
+        for (int i = 0; i < shape.npoints; i++)
+            str.append(String.format("(%s, %s),", shape.xpoints[i], shape.ypoints[i]));
+        str.deleteCharAt(str.length() - 1);
+        str.append("}");
+        return str.toString();
     }
 }
